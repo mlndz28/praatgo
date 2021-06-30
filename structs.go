4bcf8c1d932b513061311dd01abcb62683de821a
mlndz28 <mlndzodin@gmail.com>
2021-06-30 15:43:22 -0600

    docs: add descriptions for structs

--- structs.go
+++ structs.go
@@ -1,6 +1,6 @@
 package praatgo
 
-// TextGrid objects
+// TextGrid objects (contains tiers)
 type TextGrid struct {
 	FileType 	string			`json:"File type"`
 	Xmin     	float64			`json:"xmin"`
@@ -10,6 +10,7 @@ type TextGrid struct {
 	Item     	[]interface{}	`json:"item"`
 }
 
+// A connected sequence of labeled intervals
 type IntervalTier struct {
 	Class     string		`json:"class"`
 	Name      string		`json:"name"`
@@ -19,12 +20,15 @@ type IntervalTier struct {
 	Intervals []Interval	`json:"intervals"`
 }
 
+
+// Transcription for a given time range
 type Interval struct {
 	Xmin float64	`json:"xmin"`
 	Xmax float64	`json:"xmax"`
 	Text string		`json:"text"`
 }
 
+// A sequence of labeled points
 type TextTier struct {
 	Class  string	`json:"class"`
 	Name   string	`json:"name"`
@@ -34,6 +38,7 @@ type TextTier struct {
 	Points []Point	`json:"points"`
 }
 
+// An event in time
 type Point struct {
 	Number float64	`json:"number"`
 	Mark   string	`json:"mark"`
